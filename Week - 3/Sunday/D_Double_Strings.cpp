4ea17006f5a36d420aafd341b78de015893cc20d
SAYAN-DEV-NAT <sayandevnath01@gmail.com>
2024-04-05 20:54:01 +0600

    update

--- Week - 3/Sunday/D_Double_Strings.cpp
+++ Week - 3/Sunday/D_Double_Strings.cpp
@@ -3,31 +3,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t; cin >> t; while(t--) {
-        int n; cin >> n;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
         string s[n];
         unordered_map<string, bool> mp;
-        for(int i=0; i<n; i++) {
-            cin >> s[i]; mp[s[i]] = true;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> s[i];
+            mp[s[i]] = true;
         }
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             bool ok = false;
-            for(int j=1; j<s[i].length(); j++) {
+            for (int j = 1; j < s[i].length(); j++)
+            {
                 string pref = s[i].substr(0, j);
-                string suff = s[i].substr(j, s[i].length()-j);
-                if(mp[pref] && mp[suff]) {
+                string suff = s[i].substr(j, s[i].length() - j);
+                if (mp[pref] && mp[suff])
+                {
                     ok = true;
                     break;
                 }
             }
-            cout<<ok;
+            cout << ok;
         }
-        cout<<'\n';
+        cout << '\n';
     }
 
     return 0;
