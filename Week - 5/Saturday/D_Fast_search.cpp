1fce059bcee83a54abde451ca96f02de14f961e5
SAYAN-DEV-NAT <sayandevnath01@gmail.com>
2024-04-20 23:22:36 +0600

    update

---
+++ Week - 5/Saturday/D_Fast_search.cpp
@@ -0,0 +1,27 @@
+// File: D_Fast_search
+// Author: _Sayan_Dev_Nath_
+// Created: 20-04-2024  20:20:30
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a.begin(), a.end());
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+        auto x = lower_bound(a.begin(), a.end(), l);
+        auto y = upper_bound(a.begin(), a.end(), r);
+        cout << abs((x - a.begin()) - (y - a.begin())) << " ";
+    }
+    return 0;
+}
