58c1453abdf1d63350b2543d7f85c308d3309f97
SAYAN-DEV-NAT <sayandevnath01@gmail.com>
2024-05-01 07:17:24 +0600

    update

--- Week - 5/Saturday/D_Fast_search.cpp
+++ Week - 5/Saturday/D_Fast_search.cpp
@@ -1,18 +1,19 @@
-// File: D_Fast_search
-// Author: _Sayan_Dev_Nath_
-// Created: 20-04-2024  20:20:30
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // 01-05-2024  07:09:39
+#define ll long long int
 using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; i++)
         cin >> a[i];
-
     sort(a.begin(), a.end());
+
     int q;
     cin >> q;
     while (q--)
@@ -21,7 +22,7 @@ int main()
         cin >> l >> r;
         auto x = lower_bound(a.begin(), a.end(), l);
         auto y = upper_bound(a.begin(), a.end(), r);
-        cout << (y - a.begin()) - (x - a.begin()) << " ";
+        cout << (y - x) << " ";
     }
     return 0;
 }
