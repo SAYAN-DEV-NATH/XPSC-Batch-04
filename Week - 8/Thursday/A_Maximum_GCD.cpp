985a534decf12c959bf8bd04093e3cf8f8a115de
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-05-17 19:38:24 +0600

    update

---
+++ Week - 8/Thursday/A_Maximum_GCD.cpp
@@ -0,0 +1,23 @@
+// Creator: SAYAN-DEV-NATH
+// Created: 17-05-2024 00:32:37
+#include <bits/stdc++.h>
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    ll TC;
+    cin >> TC;
+    while (TC--)
+    {
+        ll n;
+        cin >> n;
+        cout << n / 2 << endl;
+    }
+
+    return 0;
+}
