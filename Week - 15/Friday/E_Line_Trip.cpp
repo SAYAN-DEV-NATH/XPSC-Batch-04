251c011987c132b79551c3d5182ae041fc081477
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-07-13 20:27:19 +0600

    update

--- Week - 15/Friday/E_Line_Trip.cpp
+++ Week - 15/Friday/E_Line_Trip.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main() {
-  ios::sync_with_stdio(false); cin.tie(0);
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   int t;
   cin >> t;
-  while (t--) {
+  while (t--)
+  {
     int n, x;
     cin >> n >> x;
     int prev = 0, ans = 0;
-    for (int i = 0; i < n; ++i) {
-      int a; cin >> a;
+    for (int i = 0; i < n; ++i)
+    {
+      int a;
+      cin >> a;
       ans = max(ans, a - prev);
       prev = a;
     }
